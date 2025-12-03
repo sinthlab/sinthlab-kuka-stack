391da4be1c735abd1249aa5ff87c091e60f9e6b4
Navin Modi <nmodi5@alumni.jh.edu>
2025-12-03 13:42:08 -0500

    adjusting dx slider range

--- sinthlab_bringup/scripts/admittance_gains_gui.py
+++ sinthlab_bringup/scripts/admittance_gains_gui.py
@@ -97,9 +97,9 @@ class AdmittanceGainsWindow(QtWidgets.QWidget):
 
         self._status_label = QtWidgets.QLabel("Ready")
 
-        self._dq_slider = SliderRow("dq_gains scale", 1, 60, 0.1)
-        self._dx_slider = SliderRow("dx_gains scale", 1, 12, 0.05)
-        self._exp_slider = SliderRow("exp_smooth", 30, 70, 0.01)
+        self._dq_slider = SliderRow("dq_gains scale", 1, 30, 0.1)
+        self._dx_slider = SliderRow("dx_gains scale", 1, 40, 0.01)
+        self._exp_slider = SliderRow("exp_smooth", 50, 99, 0.01)
 
         self._dq_slider.valueChanged.connect(self._handle_dq_scale)
         self._dx_slider.valueChanged.connect(self._handle_dx_scale)
